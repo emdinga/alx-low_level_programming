aa1979fb442d8f144c23b9613ebb0cd090a8a6c4
emdinga <Emdinga@gmail.com>
2023-02-16 13:05:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * Main-entry
- * function returns the input
+ * Main - entry point
+ * function the input
  * retuns a string
- * return: Always 0 (success)
+ * return : Always 0 (success)
  */
 
 int main(void)
 {
-	puts(" Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\' Programming is like building a multilingual puzzle\n");
+	return (0);
 }
