7ec251002d862dd46ab6acceb1341ff6ae72d7d9
emdinga <Emdinga@gmail.com>
2023-02-20 10:06:18 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,22 +3,23 @@
 #include <time.h>
 
 /**
- * main is main block
+ * main - Entry point
  * Description: combo combnination
- * return 0
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
 	int c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
-	if (c !='9')
+	if (c != '9')
 	{
 		putchar(';');
 		putchar(' ');
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
