f8bba6a43ce7f6a8cfb498a10cf635d4427c88c8
emdinga <Emdinga@gmail.com>
2023-02-21 10:25:44 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: return values
+ * Return: Always 0 (sucess)
+ */
+
+int _islower(int c)
+{
+	c = 'c'
+
+	while (c = 'c')
+	{
+		_putchar('1');
+	}
+	_putchar('0');
+	_putchar('\n');
+	return (0);
+}
