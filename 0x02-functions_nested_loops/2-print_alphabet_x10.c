6ef13acd31de1c5c804cc37ad83708c66c2da8d2
emdinga <Emdinga@gmail.com>
2023-02-21 09:27:06 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: prints alphabets 10x
+ * Return: Always 0 (sucees)
+ */
+
+void print_alphabet_x10(void);
+{
+	char c;
+	int b = 0;
+
+	while (b < 10)
+	{
+		c = 'a';
+		while (c <= b)
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n')
+	}
+}
