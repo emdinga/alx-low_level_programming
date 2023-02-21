d405dc43c1e4970f755f304e2e3b18c9e018848b
emdinga <Emdinga@gmail.com>
2023-02-21 11:37:43 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @b: initial value
+ * Return: Always a success
+ */
+
+int _abs(int b)
+{
+	if (b < 0)
+		b = -(b);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
+
