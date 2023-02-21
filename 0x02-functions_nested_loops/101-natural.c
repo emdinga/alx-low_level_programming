e31e48c54f9c8f32f6e06678e7d23826349ae17c
emdinga <Emdinga@gmail.com>
2023-02-21 13:53:34 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdioh>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *Return: o
+ */
+
+int main(void)
+{
+	int sum;
+	int i;
+
+	sum = 0;
+	for (i = 0; i < 10; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
