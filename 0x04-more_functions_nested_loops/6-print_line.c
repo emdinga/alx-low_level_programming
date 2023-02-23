5b203ba0d33013800598f96877cd7e2bcefe58a7
emdinga <Emdinga@gmail.com>
2023-02-23 08:38:14 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ * @n: parameter value
+ * Return: n
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('_');
+	}
+}
