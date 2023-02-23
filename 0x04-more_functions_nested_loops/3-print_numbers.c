f9625109aefc74a3b38e0dab1bd3b1bfd757b7f6
emdinga <Emdinga@gmail.com>
2023-02-23 05:07:59 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers -entry point
+ * Description: rints numbers
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
