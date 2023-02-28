95275e6ea81a8e6b89f5dcc49c7117f08603adc3
emdinga <Emdinga@gmail.com>
2023-02-27 17:20:15 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ * @a: parameter value
+ * Return: s
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
