11c8a756755943934e80a026e0cf67c7883b0610
emdinga <Emdinga@gmail.com>
2023-03-01 10:36:01 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - main function
+ * @dest: 1st parameter
+ * @src: 2nd parameter
+ * @n: 3rd parameter
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int l;
+	int j;
+
+	l = 0;
+
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+
+	for (j < n && src[j] != '\0')
+	{
+		dest[l] = src[j];
+		j++;
+		l++;
+	}
+	dest[l] = '\0';
+	return (dest);
+}
