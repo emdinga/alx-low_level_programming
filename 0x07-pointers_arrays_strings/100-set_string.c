c787ca15b077a93875d0205c6e567f8336dfb30b
emdinga <Emdinga@gmail.com>
2023-03-06 12:59:39 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - main function
+ * @s: parameter value
+ * @to: parameter value
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
