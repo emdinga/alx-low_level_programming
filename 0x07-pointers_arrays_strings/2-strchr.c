c3be9698e58233d3332c3dbbb9725e22a745d73f
emdinga <Emdinga@gmail.com>
2023-03-06 08:27:27 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr -main function
+ * @s: 1st parameter value
+ * @c: 2nd parameter value
+ * Return: s or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	for (; s[i] >= '\0' ; i++)
+	{
+		if (s[i] == c)
+			{
+				return (&s[i]);
+			}
+	}
+	return (0);
+}
