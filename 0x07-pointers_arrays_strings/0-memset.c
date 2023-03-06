e37d20f37035cd8afa6d4538e124b01e6c7368f7
emdinga <Emdinga@gmail.com>
2023-03-06 07:42:23 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,12 @@
  * Return: s
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
 	i = 0;
-	for (n > 0; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
