da15b42c6f1f74d9a2c6bbcbeb384a9c78b0b85d
emdinga <Emdinga@gmail.com>
2023-03-06 07:36:51 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - main function
+ * @s: 1st parameter value
+ * @b: @nd parameter value
+ * @n: @rd parameter value
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+
+	i = 0;
+	for (n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
