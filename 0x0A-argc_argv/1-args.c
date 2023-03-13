727949625891082e888f65a74647cc33dafd0ab9
emdinga <Emdinga@gmail.com>
2023-03-13 02:56:22 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
-	return (0);		
+	return (0);
 }
