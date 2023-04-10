6fa3422ae2eed12c2bcfe30455709d5ac9568459
emdinga <Emdinga@gmail.com>
2023-04-10 00:03:07 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - Main function that creates a file.
+ * @filename: pointer to a file name
+ * @text_content: pointer to a string
+ * Return: -1 or 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f, w, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(f, text_content, l);
+
+	if (f == -1 || w == -1)
+		return (-1);
+
+	close(f);
+
+	return (1);
+}
