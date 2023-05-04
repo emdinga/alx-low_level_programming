5e3eed359476f751d4fd0034b6bc6ec9041a6251
emdinga <emdinga@gmail.com>
2023-05-04 12:23:01 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements
+ * @h: Pointer to the head node 
+ *
+ * Return: The number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
