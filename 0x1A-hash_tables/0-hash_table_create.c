bcc76452c6a1ea1a1e44ae29936962d647ec846c
emdinga <emdinga@gmail.com>
2023-07-09 22:44:41 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *h;
 	unsigned long int i;
 
-	h = malloc(sizeof(has_table_t));
+	h = malloc(sizeof(hash_table_t));
 	if (h == NULL)
 		return (NULL);
 	h->array = malloc(sizeof(hash_node_t *) * size);
